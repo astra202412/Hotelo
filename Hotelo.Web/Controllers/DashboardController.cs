9c9478a9d4bf3654c1f399cb7f45de9e2ebe858d
Hic <h.elali@asadarco-dz.com>
2026-06-30 17:05:18 +0200

    Add files via upload

---
+++ Hotelo.Web/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Hotelo.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelo.Web.Controllers;
+
+[Authorize]
+public class DashboardController : Controller
+{
+    private readonly IDashboardService _dashboardService;
+
+    public DashboardController(IDashboardService dashboardService)
+    {
+        _dashboardService = dashboardService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var dashboard = await _dashboardService.GetGlobalDashboardAsync();
+        return View(dashboard);
+    }
+}
---
+++ Hotelo.Web/Controllers/ReportsController.cs
@@ -0,0 +1,26 @@
+﻿using Hotelo.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelo.Web.Controllers;
+
+[Authorize]
+public class ReportsController : Controller
+{
+    private readonly IReportService _reportService;
+
+    public ReportsController(IReportService reportService)
+    {
+        _reportService = reportService;
+    }
+
+    public async Task<IActionResult> Index(int? year, int? month)
+    {
+        var y = year ?? DateTime.Today.Year;
+        var m = month ?? DateTime.Today.Month;
+        var report = await _reportService.GetDirectionReportAsync(y, m);
+        ViewBag.Year = y;
+        ViewBag.Month = m;
+        return View(report);
+    }
+}
