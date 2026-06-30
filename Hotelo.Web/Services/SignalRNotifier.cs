367c6c8f8ad098ca5e116545c4b52d173b848554
Hic <h.elali@asadarco-dz.com>
2026-06-30 17:07:41 +0200

    Add files via upload

---
+++ Hotelo.Web/Services/SignalRNotifier.cs
@@ -0,0 +1,14 @@
+﻿using Hotelo.Core.Interfaces.Services;
+using Hotelo.Web.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Hotelo.Web.Services;
+
+public class SignalRNotifier : IRealtimeNotifier
+{
+    private readonly IHubContext<NotificationHub> _hub;
+    public SignalRNotifier(IHubContext<NotificationHub> hub) => _hub = hub;
+
+    public async Task SendToAllAsync(string method, object data)
+        => await _hub.Clients.All.SendAsync(method, data);
+}
