9e68034345a646ee23354cc3149445fd42d8db95
Hic <h.elali@asadarco-dz.com>
2026-06-30 15:59:53 +0200

    Add files via upload

---
+++ Hotelo.Common/Constants/AppConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotelo.Common.Constants;
+
+public static class AppConstants
+{
+    public const string AppName = "HOTELO";
+    public const string Version = "1.0.0";
+    public const string DefaultCulture = "fr-FR";
+    public const string SupportEmail = "admin@hotelo.local";
+    public const int DefaultPageSize = 20;
+    public const int CheckInHour = 14;
+    public const int CheckOutHour = 12;
+}
---
+++ Hotelo.Common/Constants/RoleConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace Hotelo.Common.Constants;
+
+public static class RoleConstants
+{
+    public const string Admin = "Admin";
+    public const string Directeur = "Directeur";
+    public const string Receptionniste = "Receptionniste";
+    public const string Comptable = "Comptable";
+    public const string Gouvernante = "Gouvernante";
+    public const string Technicien = "Technicien";
+    public const string RH = "RH";
+    public const string Commercial = "Commercial";
+    public const string BellDesk = "BellDesk";
+
+    public static readonly string[] AllRoles = {
+        Admin, Directeur, Receptionniste, Comptable,
+        Gouvernante, Technicien, RH, Commercial, BellDesk
+    };
+}
